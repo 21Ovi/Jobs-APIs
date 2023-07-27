51c56600df257bd95e620a3a2fde4dcfc52b5167
= <=>
2023-07-27 08:41:17 +0530

    create user controller added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,16 @@
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+
 const register = async (req, res) => {
-  res.send("Register user");
+  try {
+    const user = await User.create({ ...req.body });
+    res.status(StatusCodes.CREATED).json({ user });
+  } catch (err) {
+    console.error("Error in register:", err);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "An error occurred during registration." });
+  }
 };
 
 const login = async (req, res) => {
