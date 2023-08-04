1e1c0aed47334112abb534f63d611a3d6e7e010c
= <=>
2023-08-04 21:53:01 +0530

    get all jobs

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -7,17 +7,20 @@ const {
 } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
   res.send("get job");
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send("update job");
 };
